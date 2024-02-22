159345a558471ed74837c608effe32efb295d6e2
aviarkinfosoft <157453141+aviarkinfosoft@users.noreply.github.com>
2024-02-22 18:24:57 +0530

    day-12

---
+++ day-12/src/App.jsx
@@ -0,0 +1,22 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-12/src/Contact.jsx
@@ -0,0 +1,13 @@
+// Contact.js
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Contact information goes here</p>
+    </div>
+  );
+}
+
+export default Contact;
