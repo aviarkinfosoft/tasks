e85fb5e44fa0a47f01fb4dbf2abe4e68b6d843b2
aviarkinfosoft <157453141+aviarkinfosoft@users.noreply.github.com>
2024-03-13 18:19:22 +0530

    13 march

--- node/url/app.js
+++ node/url/app.js
@@ -5,15 +5,27 @@ const app = express();
 
 // Define handlers for different routes
 app.get('/', (req, res) => {
-    res.send('This is the homepage');
+    res.send(`
+        <h1>This is the homepage</h1>
+        <a href="/about"><button>About</button></a>
+        <a href="/contact"><button>Contact</button></a>
+    `);
 });
 
 app.get('/about', (req, res) => {
-    res.send('This is the about page');
+    res.send(`
+        <h1>This is the about page</h1>
+        <a href="/"><button>Home</button></a>
+        <a href="/contact"><button>Contact</button></a>
+    `);
 });
 
 app.get('/contact', (req, res) => {
-    res.send('This is the contact page');
+    res.send(`
+        <h1>This is the contact page</h1>
+        <a href="/"><button>Home</button></a>
+        <a href="/about"><button>About</button></a>
+    `);
 });
 
 // Default route handler for 404 Not Found
